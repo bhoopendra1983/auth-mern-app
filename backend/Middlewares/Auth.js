08502a2365a1ea9cd3c7fa055e9e61a07b780b4f
bhoopendra1983 <bhoopendra.mca2014@gmail.com>
2025-11-20 17:00:30 +0530

    Token verify

---
+++ backend/Middlewares/Auth.js
@@ -0,0 +1,23 @@
+const jwt= require('jsonwebtoken');
+
+const ensureAuthenticated = (req,res,next)=>{
+
+     const auth = req.headers['authorization'];
+        if(!auth){
+
+            return res.status(403).json({message: "Unauthorized, JWT Token is require"});
+        }
+
+         try {
+
+            const decoded = jwt.verify(auth,process.env.JWT_SECRET);
+            req.user=decoded;
+            next();
+         }
+         catch(err){
+           return res.status(403).json({message: "Unauthorized, JWT Token is wrong or expire"});
+         }
+
+}
+
+module.exports=ensureAuthenticated;
--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,12 @@ require('./Models/db');
 
 const PORT = process.env.PORT || 8080 ;
 const AuthRouter = require('./Routes/AuthRouter');
-
+const ProductRouter = require('./Routes/ProductRouter');
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/auth',AuthRouter);
+app.use('/products',ProductRouter);
+
 app.listen(PORT , ()=>{
 console.log(`Server is running on ${PORT}`)
 
